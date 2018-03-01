92c79bbd000c2c1eaf482740920fb031369a6f1c
Fahim Anwar <fahim1anwar@gmail.com>
2018-03-01 21:19:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function feelings(mood) {
+  switch(mood) {
+    case "happy":
+      return "Dance to Pharrell's 'Happy'"
+    default:
+      return "I don't recognize that mood."
+  }
+
+  console.log("Let us know how you're feeling tomorrow!")
+}
